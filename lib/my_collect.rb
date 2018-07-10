90f5df8424bc4373c55bb64a5d1d9a974e8eb67c
Scott Ungchusri <animoogle504@gmail.com>
2018-07-10 05:51:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(args)
+  if block_given?
+    iterator = 0 
+    returnArray = []
+    while iterator<args.length 
+      returnArray[iterator] = yield(args[iterator])
+      iterator+=1 
+    end
+    returnArray
+  else
+    #assert_nothing_raised
+  end
+end 
 
